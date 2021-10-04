9079d8d770b91fdc6465519ca45501caa39d2701
Sergey Vladimirov <vlsergey@gmail.com>
2021-10-04 15:06:14 +0300

    Bugfix: stack overflow in findAllByIds

--- src/test/java/com/github/vlsergey/springdata/entitysecurity/owned/OwnedEntityTest.java
+++ src/test/java/com/github/vlsergey/springdata/entitysecurity/owned/OwnedEntityTest.java
@@ -1,7 +1,9 @@
 package com.github.vlsergey.springdata.entitysecurity.owned;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
@@ -24,11 +26,13 @@ import com.github.vlsergey.springdata.entitysecurity.TestQueryListener;
 @ContextConfiguration(classes = TestConfiguration.class)
 class OwnedEntityTest {
 
-	@Autowired
-	private OwnedTestEntityRepository testRepository;
+	private static final String USERNAME_ROOT = "root";
 
 	@Autowired
 	private TestQueryListener queryListener;
+
+	@Autowired
+	private OwnedTestEntityRepository testRepository;
 
 	@AfterAll
 	static void clearSecurityContext() {
@@ -47,37 +51,20 @@ class OwnedEntityTest {
 	}
 
 	@Test
-	void testFindAll() {
+	void findAllByIdsUnderRootReturnsEmptyResultForEmptyArg() {
 		SecurityContextHolder.getContext()
-				.setAuthentication(new TestingAuthenticationToken("testUser", null, emptyList()));
+				.setAuthentication(new TestingAuthenticationToken(USERNAME_ROOT, null, emptyList()));
 
-		assertWhenDoThenQueryMatchesPattern(testRepository::findAll,
-				"^select .* from owned_test_entity .* where .*owner=\\?$");
+		assertThat(testRepository.findAllById(emptyList()), emptyCollectionOf(OwnedTestEntity.class));
 	}
 
 	@Test
-	void testFindAllUnderRoot() {
-		SecurityContextHolder.getContext().setAuthentication(new TestingAuthenticationToken("root", null, emptyList()));
-
-		assertWhenDoThenQueryMatchesPattern(testRepository::findAll, "^select .* from owned_test_entity [a-z0-9_]+$");
-	}
-
-	@Test
-	void testExistsById() {
+	void findAllByIdsUnderRootReturnsEmptyResultForUnknownIds() {
 		SecurityContextHolder.getContext()
-				.setAuthentication(new TestingAuthenticationToken("testUser", null, emptyList()));
+				.setAuthentication(new TestingAuthenticationToken(USERNAME_ROOT, null, emptyList()));
 
-		assertWhenDoThenQueryMatchesPattern(() -> testRepository.existsById(UUID.randomUUID()),
-				"^select 1 as .* from owned_test_entity .* where .*id=\\? and .*owner=\\?$");
-	}
-
-	@Test
-	void testFindByValue() {
-		SecurityContextHolder.getContext()
-				.setAuthentication(new TestingAuthenticationToken("testUser", null, emptyList()));
-
-		assertWhenDoThenQueryMatchesPattern(() -> testRepository.findByValue(42),
-				"^select .* from owned_test_entity .* where .*value=\\? and .*owner=\\?$");
+		assertThat(testRepository.findAllById(singletonList(UUID.randomUUID())),
+				emptyCollectionOf(OwnedTestEntity.class));
 	}
 
 	@Test
@@ -91,6 +78,41 @@ class OwnedEntityTest {
 		test = testRepository.save(test);
 		test = testRepository.save(test);
 		assertNotNull(test);
+	}
+
+	@Test
+	void testExistsById() {
+		SecurityContextHolder.getContext()
+				.setAuthentication(new TestingAuthenticationToken("testUser", null, emptyList()));
+
+		assertWhenDoThenQueryMatchesPattern(() -> testRepository.existsById(UUID.randomUUID()),
+				"^select 1 as .* from owned_test_entity .* where .*id=\\? and .*owner=\\?$");
+	}
+
+	@Test
+	void testFindAll() {
+		SecurityContextHolder.getContext()
+				.setAuthentication(new TestingAuthenticationToken("testUser", null, emptyList()));
+
+		assertWhenDoThenQueryMatchesPattern(testRepository::findAll,
+				"^select .* from owned_test_entity .* where .*owner=\\?$");
+	}
+
+	@Test
+	void testFindAllUnderRoot() {
+		SecurityContextHolder.getContext()
+				.setAuthentication(new TestingAuthenticationToken(USERNAME_ROOT, null, emptyList()));
+
+		assertWhenDoThenQueryMatchesPattern(testRepository::findAll, "^select .* from owned_test_entity [a-z0-9_]+$");
+	}
+
+	@Test
+	void testFindByValue() {
+		SecurityContextHolder.getContext()
+				.setAuthentication(new TestingAuthenticationToken("testUser", null, emptyList()));
+
+		assertWhenDoThenQueryMatchesPattern(() -> testRepository.findByValue(42),
+				"^select .* from owned_test_entity .* where .*value=\\? and .*owner=\\?$");
 	}
 
 }
